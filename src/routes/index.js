527ca38dba0bc82d48b69ab52b846e3ccc166a73
Montini <devmontini@gmail.com>
2022-03-29 13:51:07 -0300

    First commit

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const counrtyRouter = require('./countries.js')
+const activityRouter = require('./activities.js')
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.use('/countries', counrtyRouter)
+router.use('/activities', activityRouter)
+
+
+module.exports = router;

bcbbc6e8fac7a1605bf80eff4c87dac300877f41
Montini <devmontini@gmail.com>
2022-03-29 18:42:09 -0300

    countries api

--- src/app.js
+++ src/app.js
@@ -16,24 +16,25 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://countries-app-cli.vercel.app/"
-  ); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://countries-app-cli.vercel.app/"
+//   ); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 if (process.env.NODE_ENV === "production") {
   server.use(express.static(path.join(__dirname.slice(0, -4), "client/build")));
 }
 
+server.use(cors());
 server.use("/", routes);
 
 // Error catching endware.
